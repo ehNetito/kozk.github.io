6494aa607bd187e6f48907d8609bbbc56ecb56d7
Gabriel <cbi205322069@titlani.uam.mx>
2015-02-19 15:13:43 -0600

    Update practica5.c

--- PConcurrente/pc2015I/pr05/practica5.c
+++ PConcurrente/pc2015I/pr05/practica5.c
@@ -1,6 +1,6 @@
-/**
-Ejercicio uno. Completo
-**/
+/*
+Ejercicio uno completo
+*/
 #include<omp.h>
 #include<stdio.h>
 #include<stdlib.h>
@@ -9,23 +9,26 @@ int idA;
 int idB;
 
 void tareaA(){
-  printf("Hola soy A, mi id=#%d\n",idA);
+  printf("Hola soy A, mi id=%d\n",idA);
 }
 void tareaB(){
-  printf("Mucho gusto soy B, mi id=#%d\n",idB);
+  printf("Mucho gusto soy B, mi id=%d\n",idB);
 }
 
 int main(int argc,char *argv[]){
-	idA=omp_get_thread_num();
-	idb=omp_get_thread_num();
-	
 
 	#pragma omp parallel sections
 	{
 		#pragma omp section
+		{
+		  idA=omp_get_thread_num();
 		  tareaA();
+		}
 		#pragma omp section
+		{
+		  idB=omp_get_thread_num();
 		  tareaB();
+		}
 	}
 	return 0;
 }
