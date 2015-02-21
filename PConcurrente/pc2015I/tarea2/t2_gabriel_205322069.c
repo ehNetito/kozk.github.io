9698fc49783275e32d6b142d092d3e11274fc753
Gabriel <cbi205322069@titlani.uam.mx>
2015-02-20 22:07:21 -0600

    tarea2

---
+++ PConcurrente/pc2015I/tarea2/t2_gabriel_205322069.c
@@ -0,0 +1,82 @@
+//Todavia falta pero va mejor
+
+#include<omp.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define N 3
+
+int pet=0;
+int rsp=0;
+omp_lock_t CS;
+omp_lock_t CC;
+omp_lock_t CD;
+
+void productor(){
+  while(1){
+    omp_set_lock(&CS);
+
+    if(pet==0){
+      rsp=rsp%10;
+      rsp++;
+    }	else if(pet==1){
+      rsp=(rsp%10)+10;
+      rsp++;
+    }	else if(pet==2){
+      rsp=(rsp%10)+20;
+      rsp++;
+    }
+    int id=omp_get_thread_num();
+
+    printf("----Productor %d con petición: %d y respuesta: %d\n",id,pet,rsp);
+    
+//    omp_unset_lock(&CC); // Para el caso sin distribuidor
+    omp_unset_lock(&CD);
+    sleep(1);
+  }
+}
+
+void distribuidor(){
+  while(1){
+    omp_set_lock(&CD);
+    int id=omp_get_thread_num();
+    printf("--Distribuidor: %d, Petición = %d\n",id,pet);
+    omp_unset_lock(&CS);
+    omp_unset_lock(&CC);
+
+    sleep(1);
+  }
+}
+
+void cliente(){
+  while(1){
+    omp_set_lock(&CC);
+    pet++;
+    pet=pet%3;
+    int id=omp_get_thread_num();
+    printf("Cliente con id= %d, Petición: %d\n",pet,id);
+    omp_unset_lock(&CD);
+    sleep(1);
+  }
+}
+
+int main(int argc,char *argv[]){
+	omp_init_lock(&CS);
+	omp_init_lock(&CC);
+	omp_init_lock(&CD);
+	
+	omp_set_lock(&CS);
+	omp_set_lock(&CD);
+	omp_set_num_threads(N);
+	#pragma omp parallel shared(pet,rsp,CS,CC)
+	{
+		int id=omp_get_thread_num();
+		if(id==0)
+			cliente();
+		else if(id==1)
+			distribuidor();
+		else if(id==2)
+			productor();
+	}
+	return 0;
+}
